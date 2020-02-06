8b07e60726dc7be4828fc3152a444eb81c5061d0
luke rohde <morneau33@gmail.com>
2020-02-06 14:06:42 -0600

    binputs and button working on AddItem.jsx

--- src/components/AddItem/AddItem.jsx
+++ src/components/AddItem/AddItem.jsx
@@ -8,14 +8,36 @@ class AddItem extends Component {
       url: ''
     }
   }
-  
+
+  makeNewItem = (event, propertyValue) => {
+    console.log(this.state.newItem);
+    
+    console.log('typing into a box');
+    this.setState({
+      newItem: {
+        ...this.state.newItem,
+        [propertyValue]: event.target.value
+      }
+    })
+
+  }
+
+  addNewItem = () => {
+    console.log('clicking add item to shelf', this.state.newItem);
+    this.props.dispatch({
+      type: 'ADD_NEW_ITEM',
+      payload: this.state.newItem
+    });
+    this.props.history.push('/info');
+  }
+
   render() {
     return (
       <div>
         <p>
-          <input type="text" placeholder="item description"></input>
-          <input type="text" placeholder="item url"></input>
-          <button>add to the shelf</button>
+          <input type="text" placeholder="item description" value={this.state.newItem.description} onChange={(event) => this.makeNewItem(event, 'description')}></input>
+          <input type="text" placeholder="item url" value={this.state.newItem.url} onChange={(event) => this.makeNewItem(event, 'url')}></input>
+          <button onClick={this.addNewItem}>add to the shelf</button>
         </p>
       </div>
 
